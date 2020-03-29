ea41737ff0cdcf9a74f2bc24aec315b9749f378b
brect <b.limas93@gmail.com>
2020-03-29 19:44:45 -0300

    Configurado toolbar do app;

--- app/src/main/java/br/com/padawan/socialurbanphoto/activity/MainActivity.java
+++ app/src/main/java/br/com/padawan/socialurbanphoto/activity/MainActivity.java
@@ -1,16 +1,67 @@
 package br.com.padawan.socialurbanphoto.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import br.com.padawan.socialurbanphoto.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private FirebaseAuth auth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("urban photo");
+        setSupportActionBar( toolbar );
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch ( item.getItemId() ){
+            case R.id.menu_sair :
+                deslogarUsuario();
+                configuraLogoutActivity();
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void deslogarUsuario() {
+        try {
+            auth.signOut();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void configuraLogoutActivity() {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
